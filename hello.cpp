4bb0c2ceb29932478aa30a89c5fce41fb2a6ee52
saishab02 <soishobsarkar@gmail.com>
2025-11-10 15:16:51 +0600

    first commit

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    // std::cout is used to output data to the console (standard output)
+    std::cout << "Hello, World! This is a simple test." << std::endl;
+
+    // A return value of 0 typically indicates successful execution
+    return 0;
+}
